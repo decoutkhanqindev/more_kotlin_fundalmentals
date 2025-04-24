c6c3a0e7ac75ebd82e3989211fa50e87acc22144
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-04-24 15:13:09 +0700

    tracking app event with classes, collections and HOF

---
+++ src/tracking_event_app.kt
@@ -0,0 +1,43 @@
+enum class Daypart {
+  MORNING, AFTERNOON, EVENING
+}
+
+data class Event(
+  val title: String,
+  val description: String? = null,
+  val daypart: Daypart,
+  val durationInMinutes: Int
+)
+
+val Event.durationOfEvent: String
+  get() = if (durationInMinutes < 60) {
+    "short"
+  } else {
+    "long"
+  }
+
+fun main() {
+  val event1 =
+    Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+  val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+  val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+  val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+  val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+  val event6 =
+    Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+
+  val events: List<Event> = listOf(event1, event2, event3, event4, event5, event6)
+
+  val shortEvents: List<Event> = events.filter { it: Event -> it.durationInMinutes < 60 }
+  println("You have ${shortEvents.size} short events.")
+
+  val groupedEvents: Map<Daypart, List<Event>> = events.groupBy { it.daypart }
+  groupedEvents.forEach { (daypart: Daypart, events: List<Event>) ->
+    println("$daypart: ${events.size} events")
+  }
+
+  val lastEvent: Event = events.last()
+  println("Last event: ${lastEvent.title}")
+
+  println("Duration of first event of the day: ${events[0].durationOfEvent}")
+}
